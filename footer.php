8fe93998383dd072bc0e201fbe33b1eced59307b
Paul Revelo <Paul Revelo>
2016-02-02 17:27:29 +0800

    Third commit

--- footer.php
+++
@@ -1,9 +0,0 @@
-<!-- Main Footer -->
-<footer class="main-footer">
-  <!-- To the right -->
-  <div class="pull-right hidden-xs">
-    Developed by: Garingo & Revelo
-  </div>
-  <!-- Default to the left -->
-  <strong>Copyright &copy; 2015 <a href="#">MSU-IIT School of Graduate Studies</a>.</strong> All rights reserved.
-</footer>
