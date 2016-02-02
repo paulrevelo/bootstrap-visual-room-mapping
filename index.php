f785dfc9d9d89cea5eedd34a08f190dbb1517cfe
Paul Revelo <Paul Revelo>
2016-02-02 18:04:38 +0800

    Third commit

--- index.php
+++ index.php
@@ -31,14 +31,16 @@
   <!-- /.content-wrapper -->
 </div>
 <!-- ./wrapper -->
+<script src="http://maps.googleapis.com/maps/api/js?sensor=false&extension=.js&output=embed"></script>
+<script src="bootstrap/js/bootstrap.min.js"></script>
 
 <script src="plugins/jQuery/jQuery-2.1.4.min.js"></script>
-<script src="bootstrap/js/bootstrap.min.js"></script>
 <script src="plugins/slimScroll/jquery.slimscroll.min.js"></script>
 <script src="plugins/fastclick/fastclick.js"></script>
+
 <script src="dist/js/app.min.js"></script>
 <script src="dist/js/demo.js"></script>
 <script src="dist/js/scripts.js"></script>
-<script src="http://maps.googleapis.com/maps/api/js?sensor=false&extension=.js&output=embed"></script>
+
 </body>
 </html>
